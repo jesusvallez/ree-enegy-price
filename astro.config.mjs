509bf037d6dc772f48ef0e3014a77ea88cb0eff1
Jesus Vallez Gomis <jesusvg91@gmail.com>
2025-03-09 11:10:56 +0100

    feat(config): bundle visualizer configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,21 @@
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'astro/config'
 
+/**
+ * Uncomment the following line and visualizer line to enable bundle visualizer
+ * and run `astro build` to generate stats.html
+ */
+// import { visualizer } from 'rollup-plugin-visualizer'
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [
+      tailwindcss(),
+      // visualizer({
+      //   emitFile: true,
+      //   filename: 'stats.html',
+      // }),
+    ],
   },
 })
